3e449bc3194a069fdfe0ee2f46b78378db4ab22a
xStephx <bojkovskistefan9@gmail.com>
2025-01-30 16:33:29 +0100

    added comments to script file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,9 @@
-
+// DOM elements
 const hamburgerIcon = document.getElementById('hamburger-icon');
 const triangleMenu = document.getElementById('triangle-menu');
 const hamburgerMenu = document.getElementById('hamburger-menu');
 
+// EventListener for hamburger menu
 hamburgerIcon.addEventListener('click', () => {
     triangleMenu.classList.toggle('hidden');
     hamburgerMenu.classList.toggle('hidden');
